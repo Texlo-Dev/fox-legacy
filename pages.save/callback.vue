d72612dbce094fb3b2e2994df75ad46735374849
rTexel <rramce3@outlook.com>
2018-09-18 20:14:29 -0400

    Edit docker compose,  remove unneeded files

--- pages.save/callback.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div class="container d-flex align-items-center justify-content-center flex-column">
-  <h1 class="has-text-white has-text-centered">Logging In...</h1>
-</div>
-</template>
