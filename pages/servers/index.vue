0e3e555e5de97bd931b7f820e65323904b96298f
Rich Rancy <rramce3@outlook.com>
2018-10-04 11:05:22 +0000

    Update index.vue

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="section">
    <h1 class="title has-text-white">
-      Select A Server
+      <br><br>Select A Server
    </h1>
    <div v-if="guilds" v-for="guild in guilds" :key="guild.id" class="container" align="center">
       <b-tooltip :label="guild.name" type="is-light" position="is-right">
