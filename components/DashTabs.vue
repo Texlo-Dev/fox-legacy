4bf7df598655fc1ddbe3a57832a573cb894a64e6
rTexel <rramce3@outlook.com>
2018-10-18 06:30:23 -0400

    Push initial dashtabs.

--- components/DashTabs.vue
+++ components/DashTabs.vue
@@ -1,36 +1,41 @@
 <template>
    <div class="hero" style="background-color: #2b2f33">
-       <br class="is-hidden-touch"><br class="is-hidden-touch">
+       <br>
          <div class="hero-head">
             <div class="container">
-                <div class="columns is-centered">
-            <div class="column is-half">
-               <br>
-               <nav class="level">
-                  <div class="level-item">
-                    <figure class="image is-48x48">
+                <br>
+                 <nav class="level">
+                  <div id="guild" class="level-item">
+                        <figure class="image is-48x48">
                         <img v-if="$store.state.cachedGuild.icon" class="is-rounded" :src="`https://cdn.discordapp.com/icons/${$store.state.cachedGuild.id}/${$store.state.cachedGuild.icon}.jpg?size=128`">
                         <div v-if="!$store.state.cachedGuild.icon" class="guildicon">{{ getInitials($store.state.cachedGuild.name) }}</div>
-                     </figure>
-                     <h1 class="subtitle has-text-centered has-text-light has-text-weight-bold has-text-left">
-                        &nbsp; {{ $store.state.cachedGuild.name }}
-                     </h1>
-                  </div>
+                        </figure>
+                        <h1 class="subtitle has-text-light has-text-weight-bold has-text-left">
+                            &nbsp; {{ $store.state.cachedGuild.name }}
+                        </h1>
+                        </div>
+                      <div id="tabss" class="level-item">
+                        <b-tabs @input="redirect" v-model="selected" size="is-medium">
+                            <b-tab-item id="packages" label="Packages">
+                            </b-tab-item>
+                            <b-tab-item id="permissions" label="Permissions">
+                            </b-tab-item>
+                        </b-tabs>
+                      </div>
+                  <!--<div class="level-right">
+                      <div class="level-item has-text-centered">
+                        <b-tabs @input="redirect" v-model="selected" size="is-medium" position="is-centered">
+                            <b-tab-item id="packages" label="Packages">
+                            </b-tab-item>
+                            <b-tab-item id="permissions" label="Permissions">
+                            </b-tab-item>
+                        </b-tabs>
+                      </div>
+                  </div>-->
                </nav>
-            </div>
-            <div class="column is-half">
-               <br>
-               <b-tabs @input="redirect" v-model="selected" size="is-medium" position="is-centered">
-                  <b-tab-item id="packages" label="Packages">
-                  </b-tab-item>
-                  <b-tab-item id="permissions" label="Permissions">
-                  </b-tab-item>
-               </b-tabs>
-            </div>
-         </div>
-            </div>
          </div>
       </div>
+   </div>
 </template>
 
 <style>
@@ -66,7 +71,7 @@
 
 
 #tabss {
-    height: 125px;
+    height: 115px;
 }
 
 
