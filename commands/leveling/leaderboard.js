f1e39fcf3cbbde76be80573f5ae7584e470f3838
rTexel <rramce3@outlook.com>
2018-10-31 19:37:48 -0400

    Edit leaderboard command

--- commands/leveling/leaderboard.js
+++ commands/leveling/leaderboard.js
@@ -19,29 +19,15 @@ export default class FoxCommand extends Command {
 
     async run(message, args, prefix) {
         const data = await this.client.mongo.leveling.sort({ level: 'desc', totalXP: 'desc' }).find({ guildID: message.guild.id });
-        var mapped = data.filter(l => l.get('level')).map(c => `${c.get('userID')} ${c.get('level')} ${c.get('xp')}`).join(' ');
-        var numArr = [];
-        const mappedArr = mapped.split(' ');
-        var arr = [];
-        var xp = [];
-        var levels = [];
-        for (var u = 0; u < mappedArr.length; u += 3) {
-            arr.push(mappedArr[u]);
-        }
-        for (var l = 0; l < arr.length; l++) {
-            numArr.push(l);
-        }
-        for (var c = 1; c < mappedArr.length; c += 3) {
-            levels.push(mappedArr[c]);
-        }
-        for (var m = 2; m < mappedArr.length; m += 3) {
-            xp.push(mappedArr[m]);
-        }
+        const mapped = data.filter(d => d.get('level')).map(c => c.get());
         let page = parseInt(args[0]);
         if (!page) page = 1;
-        const paginated = this.client.paginate(numArr, page, 10);
+        const paginated = this.client.paginate(mapped, page, 10);
         let rank = 10 * (paginated.page - 1);
-        const num = await paginated.items.map(async a => `**${++rank}.** Level ${levels[a]} (${parseInt(xp[a]).toLocaleString()}) - ${(await this.client.users.fetch(arr[a])).tag}\n`);
+        const num = paginated.items.map(async mem => {
+            const check = await this.client.users.fetch(mem.userID).catch(() => null);
+            return `**${++rank}.** Level ${mem.level} (${parseInt(mem.xp).toLocaleString()}) - ${check ? check.tag : 'Deleted user'}\n`;
+        });
         const str = `${(await Promise.all(num)).join(' ')}\n${paginated.maxPage > 1 ? `To see a specific page, just run ${prefix}leaderboard [pagenumber].` : ''}`;
         const embed = new MessageEmbed()
             .setColor(this.client.brandColor)
