5522e17ce341b021faab0a1560aa1863baedd523
rramce3@outlook.com <rramce3@outlook.com>
2018-11-07 06:39:48 -0500

    Fix Permissions.

--- src/api/permissions.ts
+++ src/api/permissions.ts
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
         const obj = {};
         req.client.permissions.forEach(perm => {
             const cat = perm.category;
-            if (!obj.hasOwnProperty(cat)) obj[cat] = [];
+            if (!obj.hasOwnProperty(cat)) { obj[cat] = []; }
             obj[cat].push(perm);
         });
         return obj;
@@ -25,7 +25,7 @@ router.get("/:guildID", authMiddleware, async (req, res) => {
                 guild.perms.array();
             }
         `).catch(err => { throw err; });
-        if (!resp.filter(g => g)) throw new Error("Could Not resolve guild.");
+        if (!resp.filter(g => g)) { throw new Error("Could Not resolve guild."); }
         res.json(200, resp.filter(g => g)[0]);
     } catch (error) {
         res.json(500, { error: error.message });
@@ -35,32 +35,33 @@ router.get("/:guildID", authMiddleware, async (req, res) => {
 router.patch("/:guildID", authMiddleware, async (req, res) => {
     const { perm, status, target } = req.body;
     const { guildID } = req.params;
-    if (!target) return res.json(400, { error: "Missing or invalid parameters." });
+    if (!target) { return res.json(400, { error: "Missing or invalid parameters." }); }
     try {
         const resp = await req.client.shard.broadcastEval(`
             if (this.guilds.has('${guildID}')) {
                 const guild = this.guilds.get('${guildID}');
-                guild.perms.set('${perm}', ${JSON.stringify(target)}, '${status}').then(r => r);
+                guild.perms.add('${perm}', ${JSON.stringify(target)}, '${status}').then(r => r);
             }
         `).catch(err => { throw err; });
-        if (!resp.filter(g => g)) throw new Error("Could Not resolve guild.");
+        if (!resp.filter(g => g)) { throw new Error("Could Not resolve guild."); }
         res.json(200, resp.filter(g => g)[0]);
     } catch (error) {
+        console.error(error);
         res.json(500, { error: error.message });
     }
 });
 
 router.delete("/:guildID/:targetID", authMiddleware, async (req, res) => {
     const { targetID, guildID } = req.params;
-    if (!targetID) return res.json(400, { error: "Missing or invalid parameters." });
+    if (!targetID) { return res.json(400, { error: "Missing or invalid parameters." }); }
     try {
         const resp = await req.client.shard.broadcastEval(`
             if (this.guilds.has('${guildID}')) {
                 const guild = this.guilds.get('${guildID}');
-                guild.perms.delete('${targetID}').then(r => r);
+                guild.perms.remove('${targetID}').then(r => r);
             }
         `).catch(err => { throw err; });
-        if (!resp.filter(g => g)) throw new Error("Could Not resolve guild.");
+        if (!resp.filter(g => g)) { throw new Error("Could Not resolve guild."); }
         res.json(200, resp.filter(g => g)[0]);
     } catch (error) {
         res.json(500, { error: error.message });
